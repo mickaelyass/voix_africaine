173e26559a939dc055ead8dad834dd413cf170c6
bonheur <yassegoungbeseton@gmail.com>
2025-08-08 22:51:06 +0100

    varable d'environnement change4

--- app/middleware.py
+++ app/middleware.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def add_middleware(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-          allow_origins=["http://localhost:3000"],
+          allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
