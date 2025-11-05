f82579a4a64f98819a7b33921e7b10ea8ccfd2c6
bonheur <yassegoungbeseton@gmail.com>
2025-11-06 00:40:03 +0100

    changer l'utilisateur par defaut

--- app/middleware.py
+++ app/middleware.py
@@ -25,7 +25,7 @@ def add_middleware(app: FastAPI):
     # Configuration CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["https://voixafricaine.netlify.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
